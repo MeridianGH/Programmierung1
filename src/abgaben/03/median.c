4da6add235d50899d0597a59f592aa715ad5c402
Meridian <meridianpy@gmail.com>
2022-11-10 09:26:19 +0100

    Update median.c

--- src/abgaben/03/median.c
+++ src/abgaben/03/median.c
@@ -10,7 +10,7 @@ double arithMW(int numbers[], const size_t n) {
 
 double geoMW(int numbers[], const size_t n) {
     double result = 0;
-    for (int i = 0; i < n; ++i) { result += pow(i, 1.0 / (double)n); }
+    for (int i = 0; i < n; ++i) { result += pow(numbers[i], 1.0 / (double)n); }
     return result;
 }
 
@@ -23,11 +23,15 @@ double harmMW(int numbers[], const size_t n) {
 int main(void) {
     const size_t n = 20;
     int numbers[n];
-    for (size_t i = 0; i < n; i++) {
-        numbers[i] = rand() % 20 + 1;
+    for (size_t i = 0; i < n; i++) { numbers[i] = rand() % 20 + 1; }
+
+    for (int i = 0; i < n; ++i) {
+        printf(i == 0 ? "[ " : "");
+        printf("%d", numbers[i]);
+        printf(i != n - 1 ? ", " : " ]\n");
     }
 
-    printf("%f\n", arithMW(numbers, n));
-    printf("%f\n", geoMW(numbers, n));
-    printf("%f\n", harmMW(numbers, n));
+    printf("Arithmetic: %f\n", arithMW(numbers, n));
+    printf("Geometric:  %f\n", geoMW(numbers, n));
+    printf("Harmonic:   %f\n", harmMW(numbers, n));
 }
