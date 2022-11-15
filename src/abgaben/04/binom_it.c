31b423061241d0334dde4aa7191ee1c8aa59d20e
Meridian <meridianpy@gmail.com>
2022-11-15 13:41:56 +0100

    Update binom_it.c

--- src/abgaben/04/binom_it.c
+++ src/abgaben/04/binom_it.c
@@ -1,13 +1,56 @@
 #include <stdio.h>
+#include <limits.h>
 
+// DISCLAIMER: %llu doesn't compile under Windows when using "-Werror". This program works and compiles without errors if I omit the flag.
+// Should this not compile under Linux, please replace all occurrences of "%llu" with "%d" and cast the values to "(int)".
+// This will however obviously print wrong values.
+
+// Define commonly used type.
 typedef unsigned long long ull;
 
-ull binomCoef(ull n, ull k) {
-    if (k > n) { return 0; }
-    if (k == 0 || k == n) { return 1; }
-    return binomCoef(n - 1, k - 1) + binomCoef(n - 1, k);
+ull binomCoef(int n, int k) {
+    // Return trivial case.
+    if (n < k) { return 0; }
+    // Create empty Pascal's triangle.
+    ull c[n + 1][k + 1];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < k; j++) {
+            c[i][j] = 0;
+        }
+    }
+    // Fill triangle using addition.
+    for (int i = 0; i <= n; i++) {
+        // Fill to smaller of i or k, so we don't calculate unnecessary values.
+//        printf("%*s", 4 * (n - i),  ""); // Uncomment to print Pascal's triangle.
+        for(int j = 0; j <= (i < k ? i : k); j++) {
+            if(j == 0 || j == i) {
+                // Set edges to 1.
+                c[i][j] = 1;
+            } else {
+                // Calculate value using two values of previous line.
+                ull prev1 = c[i-1][j-1];
+                ull prev2 = c[i-1][j];
+                if (prev1 > ULONG_LONG_MAX - prev2) { return 0; }
+                c[i][j] = prev1 + prev2;
+            }
+//            printf(" %6llu ", c[i][j]); // Uncomment to print Pascal's triangle.
+        }
+//        printf("\n"); // Uncomment to print Pascal's triangle.
+    }
+    return c[n][k];
 }
 
 int main(void) {
-    printf("%d", (int)binomCoef(756, 25));
+    printf("%llu\n", binomCoef(18, 8));
+
+    int n = 0;
+    ull result = 1;
+    while (result != 0) {
+        // The binomial coefficient will be the largest, when we use (n, ⌊n/2⌋).
+        // C implicitly rounds down when we divide two integers.
+        // If we can compute this value successfully, all other values for this n will be computable as well, as they are guaranteed to be lower.
+        result = binomCoef(n, (n / 2));
+        n++;
+    }
+    printf("Largest possible a: %d\n", n);
 }
